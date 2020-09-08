cef8e0cf55cbcdda6b39b501786ed3dc296f600e
Andrei Leonard Nicusan <a.l.nicusan@bham.ac.uk>
2020-09-08 17:48:15 +0300

    removed stdout redirection as LIGGGHTS can now be silenced

--- coexist/base.py
+++ coexist/base.py
@@ -297,7 +297,6 @@ class Simulation:
         if self.verbose:
             self.simulation.command(f"run {num_steps}")
         else:
-            #with stdout_redirector(self._log):
             self.simulation.command(f"run {num_steps} post no")
 
 
@@ -312,7 +311,6 @@ class Simulation:
         if self.verbose:
             self.simulation.command(f"run {timestamp} upto")
         else:
-            #with stdout_redirector(self._log):
             self.simulation.command(f"run {timestamp} upto post no")
 
 
@@ -408,59 +406,6 @@ class Simulation:
         )
 
         return docstr
-
-
-
-
-libc = ctypes.CDLL(None)
-
-if platform.system() == 'Darwin':
-    stdout = '__stdoutp'
-else:
-    stdout = 'stdout'
-
-c_stdout = ctypes.c_void_p.in_dll(libc, stdout)
-
-
-@contextmanager
-def stdout_redirector(stream):
-    # The original fd stdout points to. Usually 1 on POSIX systems.
-    original_stdout_fd = sys.stdout.fileno()
-
-    def _redirect_stdout(to_fd):
-        """Redirect stdout to the given file descriptor."""
-        # Flush the C-level buffer stdout
-        libc.fflush(c_stdout)
-        # Flush and close sys.stdout - also closes the file descriptor (fd)
-        sys.stdout.close()
-        # Make original_stdout_fd point to the same file as to_fd
-        os.dup2(to_fd, original_stdout_fd)
-        # Create a new sys.stdout that points to the redirected fd
-        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
-
-    def _close_all():
-        tfile.close()
-        os.close(saved_stdout_fd)
-
-    # Save a copy of the original stdout fd in saved_stdout_fd
-    saved_stdout_fd = os.dup(original_stdout_fd)
-    try:
-        # Create a temporary file and redirect stdout to it
-        tfile = tempfile.TemporaryFile(mode='w+b')
-        _redirect_stdout(tfile.fileno())
-
-        # Catch SIGINT so we close all file descriptors opened
-        signal.signal(signal.SIGINT, _close_all)
-
-        # Yield to caller, then redirect stdout back to the saved fd
-        yield
-        _redirect_stdout(saved_stdout_fd)
-        # Copy contents of temporary file to the given stream
-        tfile.flush()
-        tfile.seek(0, io.SEEK_SET)
-        stream.write(tfile.read())
-    finally:
-        _close_all()
 
 
 
