cdc6f56eaa681a2bdb8bcc31593acc36e4628f3f
Andrei Leonard Nicusan <a.l.nicusan@bham.ac.uk>
2021-10-27 11:47:19 +0100

    insert coexist parent directory in sys.path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../coexist'))
+sys.path.insert(0, os.path.abspath('../..'))
 os.environ['MPLBACKEND'] = 'Agg'  # avoid tkinter import errors on rtfd.io
 
 
