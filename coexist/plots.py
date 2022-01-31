2d2e0ad1aecea038c1e3193929e08e0738fa7205
Andrei Leonard Nicusan <a.l.nicusan@bham.ac.uk>
2022-01-31 14:17:21 +0000

    added confidence intervals in the convergence plot and slices through N-dimensional space for access2d
    Former-commit-id: c99b5517010cdccbba9aee03b0cf6b670791076f

--- coexist/plots.py
+++ coexist/plots.py
@@ -81,55 +81,74 @@ def format_fig(fig, size=20, font="Computer Modern", template="plotly_white"):
     fig.update_layout(template = template)
 
 
+
+
 def access(
     access_data,
     select = lambda results: results[:, -1] < np.inf,
     epochs = ...,
     colors = px.colors.qualitative.Set1,
-    overall = True,
+    overall = False,
+    means = True,
+    confidence = True,
 ):
     '''Create a Plotly figure showing the solutions tried, uncertainties and
     error values found in a `coexist.Access` run.
 
     Parameters
     ----------
-    access_data: coexist.AccessData
+    access_data : coexist.AccessData or str
         An `AccessData` object containing all information about an ACCES run;
         you can initialise it with ``coexist.AccessData.read("folder_path")``.
+        Alternatively, supply the ``folder_path`` directly.
 
-    select: function, default lambda results: results[:, -1] < np.inf
+    select : function, default lambda results: results[:, -1] < np.inf
         A filtering function used to plot only selected solutions tried, based
         on an input 2D table `results`, with columns formatted as [param1,
         param2, ..., param1_std, param2_std, ..., overall_std, error_value].
         E.g. to only plot solutions with an error value smaller than 100:
-        `select = lambda results: results[:, -1] < 100`.
+        ``select = lambda results: results[:, -1] < 100``.
 
-    epochs: int or iterable or Ellipsis, default Ellipsis
+    epochs : int or iterable or Ellipsis, default Ellipsis
         The index or indices of the epochs to plot. An `int` signifies a single
         epoch, an iterable (list-like) signifies multiple epochs, while an
         Ellipsis (`...`) signifies all epochs.
 
-    colors: list[str], default plotly.express.colors.qualitative.Set1
+    colors : list[str], default plotly.express.colors.qualitative.Set1
         A list of colors used for each parameter plotted.
 
-    overall: bool, default True
-        If `True`, also plot the overall standard deviation.
+    overall : bool, default False
+        If `True`, also plot the overall standard deviation progression; note
+        that sometimes all parameters converge but the overall std-dev remains
+        high.
+
+    means : bool, default True
+        If `True`, also plot the centre of the region explored by CMA-ES.
+
+    confidence : bool, default True
+        If `True`, also plot the standard deviation of each parameter as
+        confidence intervals.
 
     Returns
     -------
-    fig: plotly.graph_objs.Figure
+    plotly.graph_objs.Figure
         A Plotly figure containing subplots with the solutions tried. Call the
         `.show()` method to display it.
 
     Examples
     --------
     If `coexist.Access(filepath, random_seed = 12345)` was run, the
-    directory "access_info_012345" would have been created. Plot its results:
+    directory "access_seed12345" would have been created. Plot its results:
 
     >>> import coexist
-    >>> data = coexist.AccessData.read("access_info_012345")
+    >>> data = coexist.AccessData.read("access_seed12345")
     >>> fig = coexist.plots.access(data)
     >>> fig.show()
+
+    Or more tersely:
+
+    >>> import coexist
+    >>> coexist.plots.access("access_seed12345").show()
 
     Only plot solution combinations that yielded an error value < 100:
 
@@ -137,12 +156,11 @@ def access(
     >>>      data,
     >>>      select = lambda results: results[:, -1] < 100,
     >>> ).show()
-
     '''
 
     # Type-checking inputs
     if not isinstance(access_data, coexist.AccessData):
-        access_data = coexist.read_access(access_data)
+        access_data = coexist.AccessData.read(access_data)
 
     # Check if sample_indices is an iterable collection (list-like)
     # otherwise just "iterate" over the single number or Ellipsis
@@ -152,17 +170,17 @@ def access(
     # Extract data needed from `access_data`
     parameters = access_data.parameters
 
-    # The data columns: [param1, param2, ..., param1_stddev, param2_stddev,
-    # ..., overall_std_dev, error]
+    # The data columns: [param1, param2, ..., error]
     results = access_data.results.to_numpy()
-    results_scaled = access_data.results_scaled.to_numpy()
 
-    ns = access_data.num_solutions
+    epochs_unscaled = access_data.epochs.to_numpy()
+    epochs_scaled = access_data.epochs_scaled.to_numpy()
+
+    ns = access_data.population
     num_epochs = access_data.num_epochs
 
     # The number of parameters
-    num_parameters = (results.shape[1] - 2) // 2
-
+    num_parameters = len(parameters)
     names = parameters.index
 
     # Create a subplots grid
@@ -176,7 +194,7 @@ def access(
     )
 
     # Plot the parameter values checked per epoch
-    num_epochs = results.shape[0] // ns
+    num_epochs = access_data.num_epochs
     epochs_params = np.repeat(np.arange(num_epochs), ns)
 
     # Filter results plotted based on the error value and selected epochs
@@ -186,7 +204,7 @@ def access(
         for e in missing:
             selection[e * ns:e * ns + ns] = False
 
-    # Compute relative error between 0 and 2
+    # Compute relative error between 0 and 1
     error = results[selection, -1]
     relative_error = (error - error.min()) / (error.max() - error.min())
 
@@ -219,12 +237,64 @@ def access(
             col = col,
         )
 
-        # Plot the standard deviations and results on the bottom row
+        # Plot the unscaled means and std-dev as confidence intervals
+        mu = epochs_unscaled[selection[::ns], i]
+
+        if means:
+            fig.add_trace(
+                go.Scatter(
+                    x = epochs_params[selection][::ns],
+                    y = mu,
+                    mode = "lines",
+                    line = dict(
+                        width = 2,
+                        color = color,
+                    ),
+                    showlegend = False,
+                ),
+                row = row,
+                col = col,
+            )
+
+        # Add transparency to confidence interval colour
+        if confidence:
+            color_alpha = color.replace("rgb", "rgba").split(")")[0] + ",0.2)"
+            std_x = epochs_params[selection][::ns]
+            std_lo = mu - epochs_unscaled[selection[::ns], i + num_parameters]
+            std_hi = mu + epochs_unscaled[selection[::ns], i + num_parameters]
+
+            fig.add_trace(
+                go.Scatter(
+                    x = std_x,
+                    y = std_lo,
+                    line_width = 0,
+                    hoverinfo = "skip",
+                    showlegend = False,
+                ),
+                row = row,
+                col = col,
+            )
+
+            fig.add_trace(
+                go.Scatter(
+                    x = std_x,
+                    y = std_hi,
+                    line_width = 0,
+                    fill = 'tonexty',
+                    fillcolor = color_alpha,
+                    hoverinfo = "skip",
+                    showlegend = False,
+                ),
+                row = row,
+                col = col,
+            )
+
+        # Plot the scaled standard deviations and results on the bottom row
         fig.add_trace(
             go.Scatter(
                 name = names[i],
-                x = epochs_params[selection],
-                y = results_scaled[selection, i + num_parameters],
+                x = epochs_params[selection][::ns],
+                y = epochs_scaled[selection[::ns], i + num_parameters],
                 mode = "lines",
                 line = dict(
                     color = color,
@@ -278,10 +348,6 @@ def access(
         fig.layout[xaxis].update(title = "Epoch")
         fig.layout[yaxis].update(title = names[i])
 
-        if parameters is not None:
-            param = parameters.iloc[i]
-            fig.layout[yaxis].update(range = [param["min"], param["max"]])
-
     # Set axis labels for the standard devation and error value subplots
     fig.layout[f"xaxis{num_parameters + 1}"].update(title = "Epoch")
     fig.layout[f"xaxis{num_parameters + 2}"].update(title = "Epoch")
@@ -292,76 +358,81 @@ def access(
     fig.layout[f"yaxis{num_parameters + 2}"].update(title = "Error Value")
 
     format_fig(fig)
+    fig.update_layout(title = dict(
+        text = "ACCES Convergence Plot",
+        font_size = 25,
+    ))
 
     return fig
 
 
 def access2d(
     access_data,
-    resolution = (1000, 1000),
-    columns = [0, 1],
+    resolution = (500, 500),
+    width = 0.2,
     select = lambda results: results[:, -1] < np.inf,
     epochs = ...,
     colorscale = "Blues_r",
     seeds = True,
 ):
-    '''Create a Plotly figure showing a 2D Voronoi diagram of the error values
-    found in a `coexist.Access` run.
-
-    This can be used to visualise a 2D optimisation problem, or a slice through
-    higher-dimensional error functions.
+    '''Create a Plotly figure showing 2D Voronoi diagram of the error values
+    found in 2D slices of the parameters explored in a `coexist.Access` run.
 
     Parameters
     ----------
-    access_data: coexist.AccessData
+    access_data : coexist.AccessData or str
         An `AccessData` object containing all information about an ACCES run;
         you can initialise it with ``coexist.AccessData.read("folder_path")``.
+        Alternatively, supply the ``folder_path`` directly.
 
-    resolution: 2-tuple, default (1000, 1000)
+    resolution : 2-tuple, default (1000, 1000)
         The number of pixels in the heatmap / Voronoi diagram shown in the
         x- and y-dimensions.
 
-    columns: 2-list, default [0, 1]
-        The columns in the ACCES history file to plot, corresponding to the
-        free parameter indices; e.g. [0, 1] represents the first two
-        parameters.
+    width : float, default 0.1
+        The width of the slices as a ratio of the parameter range.
 
-    select: function, default lambda results: results[:, -1] < np.inf
+    select : function, default lambda results: results[:, -1] < np.inf
         A filtering function used to plot only selected solutions tried, based
         on an input 2D table `results`, with columns formatted as [param1,
         param2, ..., param1_std, param2_std, ..., overall_std, error_value].
         E.g. to only plot solutions with an error value smaller than 100:
         `select = lambda results: results[:, -1] < 100`.
 
-    epochs: int or iterable or Ellipsis, default Ellipsis
+    epochs : int or iterable or Ellipsis, default Ellipsis
         The index or indices of the epochs to plot. An `int` signifies a single
         epoch, an iterable (list-like) signifies multiple epochs, while an
         Ellipsis (`...`) signifies all epochs.
 
-    colorscale: str, default "Blues_r"
+    colorscale : str, default "Blues_r"
         The colorscale used to colour-code the error value. For a list of
         possible colorscales, see `plotly.com/python/builtin-colorscales`.
 
-    seeds: bool, default True
+    seeds : bool, default True
         If True, also plot the points representing parameter combinations
         tried.
 
     Returns
     -------
-    fig: plotly.graph_objs.Figure
+    plotly.graph_objs.Figure
         A Plotly figure containing subplots with the solutions tried. Call the
         `.show()` method to display it.
 
     Examples
     --------
     If `coexist.Access(filepath, random_seed = 12345)` was run, the
-    directory "access_info_012345" would have been created. Plot its results:
+    directory "access_seed12345" would have been created. Plot its results:
 
     >>> import coexist
     >>>
-    >>> data = coexist.AccessData.read("access_info_012345")
+    >>> data = coexist.AccessData.read("access_seed12345")
     >>> fig = coexist.plots.access2d(data)
     >>> fig.show()
+
+    Or more tersely:
+
+    >>> import coexist
+    >>> coexist.plots.access2d("access_seed12345").show()
 
     Only plot the results from epochs 5, 6, 7:
 
@@ -375,12 +446,11 @@ def access2d(
     >>>     columns = [0, 2]
     >>>     select = lambda res: (res[:, 1] > 0.4) & (res[:, 1] < 0.6),
     >>> ).show()
-
     '''
 
     # Type-checking inputs
     if not isinstance(access_data, coexist.AccessData):
-        access_data = coexist.read_access(access_data)
+        access_data = coexist.AccessData.read(access_data)
 
     # Check if sample_indices is an iterable collection (list-like)
     # otherwise just "iterate" over the single number or Ellipsis
@@ -389,13 +459,28 @@ def access2d(
 
     # Extract data needed from `access_data`
     parameters = access_data.parameters
+    num_params = len(parameters)
+    names = parameters.index
 
-    # The data columns: [param1, param2, ..., param1_stddev, param2_stddev,
-    # ..., overall_std_dev, error]
+    # The data columns: [param1, param2, ..., error]
     results = access_data.results.to_numpy()
+    epochs_raw = access_data.epochs.to_numpy()
 
-    ns = access_data.num_solutions
+    ns = access_data.population
     num_epochs = access_data.num_epochs
+
+    # Create a subplots grid
+    ncols = num_params - 1
+    nrows = num_params - 1
+
+    fig = make_subplots(
+        rows = nrows,
+        cols = ncols,
+        shared_xaxes = True,
+        shared_yaxes = True,
+        horizontal_spacing = 0.1 / 4 / ncols,
+        vertical_spacing = 0.2 / 4 / nrows,
+    )
 
     # Filter results plotted based on the error value and selected epochs
     selection = select(results)
@@ -408,51 +493,112 @@ def access2d(
         for e in missing:
             selection[e * ns:e * ns + ns] = False
 
+    results = results[selection]
+    epochs_raw = epochs_raw[selection[::ns]]
+
     # Create a 2D map of pixels coloured by the closest measured point's error
-    x = np.linspace(parameters["min"][0], parameters["max"][0], resolution[0])
-    y = np.linspace(parameters["min"][1], parameters["max"][1], resolution[1])
-
-    xx, yy = np.meshgrid(x, y)
-    error_map = NearestNDInterpolator(
-        results[selection][:, columns],
-        results[selection][:, -1],
-    )(xx, yy)
-
-    # Plot the 2D error map
-    fig = go.Figure()
-    fig.add_trace(
-        go.Heatmap(
-            x = x,
-            y = y,
-            z = error_map,
-            colorscale = colorscale,
-            colorbar_title = "Error Value",
-        )
+    error = results[:, -1]
+    error_bounds = [error.min(), error.max()]
+    error_scaled = (
+        (error - error_bounds[0]) /
+        (error_bounds[1] - error_bounds[0])
     )
 
-    # Plot points tried
-    if seeds:
-        errors = results[selection][:, -1]
-        errors_scaled = (errors - errors.min()) / (errors.max() - errors.min())
+    # Plot a lower triangular matrix without the diagonal, so for 3 parameters
+    # => lower 2x2 triangle
+    for i in range(1, num_params):
+        for j in range(i):
+            row = i
+            col = j + 1
 
-        # Use inverted colorscale for good contrast (i.e. color = 1 / errors)
-        fig.add_trace(
-            go.Scatter(
-                x = results[selection][:, columns[0]],
-                y = results[selection][:, columns[1]],
-                mode = "markers",
-                marker = dict(
-                    size = 1 + 10 * errors_scaled,
-                    color = 1 / (errors - errors.min() + 1),
-                    colorscale = colorscale,
+            # Create a 2D error map with each pixel mappend to the closest
+            # sample's error
+            x = np.linspace(parameters["min"][i], parameters["max"][i],
+                            resolution[0])
+            y = np.linspace(parameters["min"][j], parameters["max"][j],
+                            resolution[1])
+
+            # Select parameter space slice of given `width`
+            cond = np.full(len(results), True)
+            others = set(range(num_params)) - {i, j}
+            for o in others:
+                param_values = results[:, o]
+                param_range = parameters[["min", "max"]].iloc[o]
+                param_range = param_range[1] - param_range[0]
+                mean = epochs_raw[-1, o]
+                cond = cond & (
+                    (param_values > mean - 0.5 * width * param_range) &
+                    (param_values < mean + 0.5 * width * param_range)
                 )
-            )
-        )
 
-    fig.update_xaxes(title = parameters.index[0],
-                     range = [parameters["min"][0], parameters["max"][0]])
-    fig.update_yaxes(title = parameters.index[1],
-                     range = [parameters["min"][1], parameters["max"][1]])
+            xx, yy = np.meshgrid(x, y)
+            error_map = NearestNDInterpolator(
+                results[cond][:, [i, j]],
+                error[cond],
+            )(xx, yy)
+
+            # Plot the 2D error map
+            fig.add_trace(
+                go.Heatmap(
+                    x = x,
+                    y = y,
+                    z = error_map,
+                    zmin = error_bounds[0],
+                    zmax = error_bounds[1],
+                    colorscale = colorscale,
+                    colorbar_title = "Error",
+                    showscale = row == col == 1,
+                    showlegend = False,
+                ),
+                row = row,
+                col = col,
+            )
+
+            # Plot points tried. Use inverted colorscale for good contrast
+            # (i.e. color = 1 / errors)
+            if seeds:
+                fig.add_trace(
+                    go.Scatter(
+                        x = results[cond, i],
+                        y = results[cond, j],
+                        mode = "markers",
+                        marker = dict(
+                            size = 1 + 10 * error_scaled,
+                            color = 1 / (error - error_bounds[0] + 1),
+                            colorscale = colorscale,
+                            colorbar_title = None,
+                        ),
+                        showlegend = False,
+                    ),
+                    row = row,
+                    col = col,
+                )
+
+            # Set axis labels
+            isub = (row - 1) * ncols + col
+            xaxis = "xaxis" if isub == 1 else f"xaxis{isub}"
+            yaxis = "yaxis" if isub == 1 else f"yaxis{isub}"
+
+            bounds = parameters[["min", "max"]]
+            fig.layout[yaxis].update(range = bounds.iloc[i].to_numpy())
+            fig.layout[xaxis].update(range = bounds.iloc[j].to_numpy())
+
+            if col == 1:
+                fig.layout[yaxis].update(title = names[i])
+
+            if row == nrows:
+                fig.layout[xaxis].update(title = names[j])
+
 
     format_fig(fig)
+    # fig.update_xaxes(showgrid = False, zeroline = False)
+    # fig.update_yaxes(showgrid = False, zeroline = False)
+    fig.update_layout(title = dict(
+        text = (
+            "ACCES Voronoi Plot - Parameter Slices Width = "
+            f"{width * 100:3.1f}% Data Range"
+        ),
+        font_size = 25,
+    ))
+
     return fig
